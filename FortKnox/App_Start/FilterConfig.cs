d394660fa8836f28991f3e3a3ea4ef87b82928b0
Bauer <torocesar@hotmail.com>
2018-10-06 15:14:31 -0500

    Add project files.

---
+++ FortKnox/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FortKnox
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
